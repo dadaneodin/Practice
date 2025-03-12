833e8ad7e954c37052eb28b5367fc453fdd5f9db
Arcwarden10 <gortim10@gmail.com>
2025-03-12 18:37:11 +0300

    creating a new project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using Microsoft.Unity.VisualStudio.Editor;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Image = UnityEngine.UI.Image;
+
+
+
+public class GameManager : MonoBehaviour
+{
+    public ImageTimer HarvestTimer;
+    public ImageTimer eatingTimer;
+    
+    public Image RaidTimerImg;
+    public Image PeasantTimerImg;
+    public Image WarriorTimerImg;
+    
+
+    public Button peasantButton;
+    public Button warriorButton;
+
+
+    public TMP_Text resoucesText;
+
+    public int peasantCount;
+    public int warriorsCount;
+    public int wheatCount;
+
+    public int wheatPerPeasant;
+    public int wheatToWarriors;
+    
+    public int peasantCost;
+    public int warriorCost;
+
+    public float peasantCreateTime;
+    public float warriorCreateTime;
+    public float raidMaxTime;
+    public int raidIncrease;
+    public int nextRaid;
+
+    private float peasantTimer = -2;
+    private float warriorTimer = -2;
+    private float raidTimer;
+
+
+    void Start()
+    {
+        UpdateText();
+    }
+
+   
+    void Update()
+    {
+        if (HarvestTimer.Tick)
+        {
+            wheatCount += peasantCount * wheatPerPeasant;
+        }
+
+        if (eatingTimer.Tick)
+        {
+            wheatCount -= warriorsCount * wheatToWarriors;
+        }
+        UpdateText();
+    }
+
+    public void CreatePeasant()
+    {
+
+    }
+
+    public void CreateWarrior()
+    {
+        
+    }
+
+    private void UpdateText()
+    {
+        resoucesText.text = peasantCount + "\n" + warriorsCount + "\n\n" + wheatCount;
+    }
+}
---
+++ Assets/harvestTimer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+using UnityEngine.UI;
+using Image = UnityEngine.UI.Image;
+
+public class HarvestTimer : MonoBehaviour
+{
+    public float MaxTime;
+    public  bool Tick;
+
+    private Image img;
+    private float currentTime;
+
+        void Start()
+    {
+        img = GetComponent<Image>();
+        currentTime = MaxTime;
+    }
+
+    void Update()
+    {
+        Tick = false;
+        currentTime -= Time.deltaTime;
+
+        if(currentTime <= 0)
+        {
+            Tick = true;
+            currentTime = MaxTime;
+        }
+        
+        img.fillAmount = currentTime/MaxTime;
+             
+    }
+}
